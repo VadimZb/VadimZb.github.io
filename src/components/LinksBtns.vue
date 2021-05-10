55ef43145260eb987ca766b91872ff1ce38d8dea
VadimZb <vvz.dev@gmail.com>
2021-05-10 22:04:55 +0300

    📦 NEW: Card links component created

---
+++ src/components/LinksBtns.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="d-flex justify-center">
+        <v-btn
+            elevation="12"
+            class="mx-3 white--text"
+            :href="project.repo"
+            color="blue-grey darken-2"
+        >
+            <v-icon left>mdi-github</v-icon>
+            Github
+        </v-btn>
+        <v-btn elevation="12" class="mx-3" :href="project.url" color="primary">
+            <v-icon left>mdi-open-in-new</v-icon>
+            Open
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        project: {
+            type: Object,
+        },
+    },
+};
+</script>
