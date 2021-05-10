5e269992fd35f01488ea1a09c741e39141b50560
VadimZb <vvz.dev@gmail.com>
2021-05-10 23:29:36 +0300

    📦 NEW: Welcome Buttons created

---
+++ src/components/WelcomeBtns.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <v-btn
+            href="https://github.com/VadimZb"
+            class="mx-3 white--text"
+            color="blue-grey darken-2"
+        >
+            <v-icon left>mdi-github</v-icon>
+            Github
+        </v-btn>
+        <v-btn
+            class="mx-3 white--text"
+            color="blue-grey darken-2"
+            @click="scrollToProjects"
+        >
+            <v-icon left>mdi-xml</v-icon>
+            Projects
+        </v-btn>
+        <v-btn
+            @click="switchTheme"
+            class="mx-3 white--text"
+            color="blue-grey darken-2"
+        >
+            <v-icon left>{{ themeIcon }}</v-icon>
+            Dark Theme: {{ themeTitle }}
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        scrollToProjects() {
+            document
+                .getElementById("projects")
+                .scrollIntoView({ behavior: "smooth" });
+        },
+        switchTheme() {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        },
+    },
+    computed: {
+        themeIcon() {
+            return this.$vuetify.theme.dark
+                ? "mdi-weather-night"
+                : "mdi-weather-sunny";
+        },
+        themeTitle() {
+            return this.$vuetify.theme.dark ? "On" : "Off";
+        },
+    },
+};
+</script>
+
+<style></style>
