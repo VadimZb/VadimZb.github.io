08c1a06dc559eb95a96e254406070b6d0f26ddd2
VadimZb <vvz.dev@gmail.com>
2021-05-10 23:28:56 +0300

    📦 NEW: HTML (XML) Brackets component

---
+++ src/components/HtmlBrackets.vue
@@ -0,0 +1,22 @@
+<template>
+    <p class="text-h4 light-green--text font-weight-bold my-0 mx-2">
+        &lt;<span v-if="closing">/</span>{{ tag }}&gt;
+    </p>
+</template>
+
+<script>
+export default {
+    props: {
+        closing: {
+            type: Boolean,
+            default: false,
+        },
+        tag: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped></style>
