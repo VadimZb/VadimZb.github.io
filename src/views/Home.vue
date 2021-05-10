45f2b24567b2d5ebb739624a3f5ccaf6cbc8e5ec
VadimZb <vvz.dev@gmail.com>
2021-05-10 22:05:38 +0300

    👌 IMPROVE: Components imported

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <hello-world />
+    <v-container fluid>
+        <div v-for="(project, i) in projects" :key="i">
+            <Card :project="project" class="custom-margin" />
+        </div>
+    </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
+import Card from "@/components/Card";
+export default {
+    name: "Home",
 
     components: {
-      HelloWorld,
+        Card,
     },
-  }
+    data() {
+        return {
+            projects: this.$store.state.projects.projects,
+        };
+    },
+};
 </script>
+
+<style scoped>
+.custom-margin {
+    margin: 10rem 0;
+}
+</style>
