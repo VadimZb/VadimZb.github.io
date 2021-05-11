560068805a4f71579c745190f5a813c36812e4b8
VadimZb <vvz.dev@gmail.com>
2021-05-11 18:16:59 +0300

    👌 IMPROVE: Accent color for each theme added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                accent: colors.deepPurple,
+                accentBg: colors.deepPurple,
+            },
+            dark: {
+                accent: colors.lightGreen,
+                accentBg: colors.lightGreen.darken3,
+            },
+        },
+    },
 });
