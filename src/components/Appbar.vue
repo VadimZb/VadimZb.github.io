f4686f1a54db7b8e89641ecd2a0b81c0b678c1a6
VadimZb <vvz.dev@gmail.com>
2021-05-11 18:16:29 +0300

    📦 NEW: Appbar component created

---
+++ src/components/Appbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app-bar fixed elevation="0" color="transparent">
+        <v-spacer></v-spacer>
+        <v-btn :to="btn.path">
+            <v-icon>{{ btn.icon }}</v-icon>
+            {{ btn.title }}
+        </v-btn>
+        <v-spacer></v-spacer>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            btn: {
+                title: "Back",
+                icon: "mdi-arrow-left",
+                path: "/",
+            },
+        };
+    },
+};
+</script>
+
+<style></style>
