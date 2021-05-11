59b6663acb341c80895bdca0bbe4c0cfa1ed448b
VadimZb <vvz.dev@gmail.com>
2021-05-11 23:17:45 +0300

    👌 IMPROVE: Timeline added; margin reduced

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,22 @@
     <div>
         <Welcome />
         <v-container fluid id="projects">
-            <div v-for="(project, i) in projects" :key="i">
-                <Card :project="project" class="custom-margin" />
+            <div v-if="$vuetify.breakpoint.mobile">
+                <div v-for="(project, i) in projects" :key="i">
+                    <Card :project="project" class="custom-margin" />
+                </div>
             </div>
+            <v-timeline dense v-else>
+                <v-timeline-item
+                    icon="mdi-xml"
+                    large
+                    color="accent"
+                    v-for="(project, i) in projects"
+                    :key="i"
+                >
+                    <Card :project="project" class="custom-margin" />
+                </v-timeline-item>
+            </v-timeline>
         </v-container>
     </div>
 </template>
@@ -29,6 +42,6 @@ export default {
 
 <style scoped>
 .custom-margin {
-    margin: 10rem 0;
+    margin: 7rem 0;
 }
 </style>
