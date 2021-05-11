c2131ffed10b3c5ea7481f2f298198db527a9f64
VadimZb <vvz.dev@gmail.com>
2021-05-11 18:17:15 +0300

    👌 IMPROVE: Appbar added on about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <Appbar />
+        <h1>This is an about page</h1>
+    </div>
 </template>
+
+<script>
+import Appbar from "@/components/Appbar";
+export default {
+    name: "About",
+
+    components: {
+        Appbar,
+    },
+};
+</script>
