23c52648a9833b85089c563fe300b844dcc88ffd
VadimZb <vvz.dev@gmail.com>
2021-05-12 00:25:22 +0300

    📦 NEW: Skills component added

---
+++ src/components/Skills.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-row>
+        <v-col
+            v-for="(lang, i) in languages"
+            :key="i"
+            cols="6"
+            class="d-flex align-center justify-center"
+        >
+            <div
+                class="d-flex flex-column justify-center align-center full-width"
+            >
+                <v-icon
+                    class="mb-2"
+                    x-large
+                    :color="tools[lang.name].color"
+                    v-text="tools[lang.name].icon"
+                ></v-icon>
+                <div class="d-flex flex-column justify-center align-center">
+                    <p class="text-h4" v-text="lang.name"></p>
+                    <p class="text-h6 font-weight-light">
+                        Skill level:
+                        <Accent class="font-weight-bold">{{
+                            tools[lang.name].knowledge
+                        }}</Accent>
+                    </p>
+                </div>
+                <ToolsStack :cols="2" :stack="lang.skills" />
+            </div>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import ToolsStack from "@/components/ToolsStack";
+import Accent from "@/components/Accent";
+export default {
+    components: {
+        Accent,
+        ToolsStack,
+    },
+    data() {
+        return {
+            tools: this.$store.state.tools,
+            languages: [
+                {
+                    name: "Javascript",
+                    skills: ["Node.js", "Vue.js", "GraphQL", "Vuetify"],
+                },
+                {
+                    name: "Typescript",
+                    skills: [],
+                },
+                {
+                    name: "Python",
+                    skills: ["Django", "Telegram Bots"],
+                },
+                {
+                    name: "Elixir",
+                    skills: [],
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style scoped>
+.full-width {
+    width: 100%;
+}
+</style>
