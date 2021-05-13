2e63732e5b0bfbfd1427ea4b26bc0bb3a7934026
VadimZb <vvz.dev@gmail.com>
2021-05-14 00:00:22 +0300

    👌 IMPROVE: Component renamed to Tag and improved

---
+++ src/components/Tag.vue
@@ -0,0 +1,27 @@
+<template>
+    <span class="d-flex align-center">
+        <span :class="color" class="text-h4 font-weight-bold my-0 mx-2">
+            &lt;{{ tag }}&gt;
+        </span>
+        <slot></slot>
+        <span :class="color" class="text-h4 font-weight-bold my-0 mx-2">
+            &lt;/{{ tag }}&gt;
+        </span>
+    </span>
+</template>
+
+<script>
+export default {
+    props: {
+        tag: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            color: "accent--text",
+        };
+    },
+};
+</script>
