0c4b4c1510161d998cc2da469056c9ce524cebaf
VadimZb <vvz.dev@gmail.com>
2021-05-16 12:43:50 +0300

    📦 NEW

---
+++ src/components/BracketsTitle.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="text-h2 text-center mb-10">
+        <Accent>{</Accent> <slot></slot> <Accent>}</Accent>
+    </h1>
+</template>
+
+<script>
+import Accent from "@/components/Accent";
+export default {
+    components: {
+        Accent,
+    },
+};
+</script>
